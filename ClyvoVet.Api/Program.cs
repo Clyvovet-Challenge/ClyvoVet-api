ec1c6f5bc2d63f382422e070cc5db87653ca3f39
pedrinzz10 <opedro485@gmail.com>
2026-05-17 21:11:48 -0300

    feat: registrar repositories no container de injecao de dependencia
    Adiciona AddScoped para os cinco repositories no Program.cs, vinculando
    cada interface a sua implementacao concreta.

--- ClyvoVet.Api/Program.cs
+++ ClyvoVet.Api/Program.cs
@@ -1,4 +1,6 @@
 using ClyvoVet.Api.Data;
+using ClyvoVet.Api.Repositories;
+using ClyvoVet.Api.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,12 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<ISugestaoProdutoRepository, SugestaoProdutoRepository>();
+builder.Services.AddScoped<ILembreteRepository, LembreteRepository>();
+builder.Services.AddScoped<IEventoPetRepository, EventoPetRepository>();
+builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
 
 var app = builder.Build();
 
