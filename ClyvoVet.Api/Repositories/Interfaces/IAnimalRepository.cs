5ef847af42cdb38c0fa233977b8e56c2e73b047b
leojp04 <leojp04@gmail.com>
2026-08-25 22:41:24 -0300

    chore: remove metodos nao usados de AnimalRepository

--- ClyvoVet.Api/Repositories/Interfaces/IAnimalRepository.cs
+++ ClyvoVet.Api/Repositories/Interfaces/IAnimalRepository.cs
@@ -4,9 +4,5 @@ namespace ClyvoVet.Api.Repositories.Interfaces;
 
 public interface IAnimalRepository
 {
-    Task<IEnumerable<Animal>> GetAllAsync(int page, int pageSize, string? tutorId);
     Task<Animal?> GetByIdAsync(string id);
-    Task<Animal> CreateAsync(Animal animal);
-    Task<Animal?> UpdateAsync(string id, Animal animal);
-    Task<bool> DeleteAsync(string id);
 }
